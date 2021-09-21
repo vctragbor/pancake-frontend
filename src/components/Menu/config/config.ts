cbb1cc9ef87e6617920c4c232ddef5321c392633
ChefCorgi <corgi.farmer@gmail.com>
2021-09-21 16:30:44 +0200

    feat(pancake squad): Add event step section

--- src/components/Menu/config/config.ts
+++ src/components/Menu/config/config.ts
@@ -64,6 +64,10 @@ const config: (t: ContextApi['t']) => ConfigMenuItemsType[] = (t) => [
         label: t('Collections'),
         href: `${nftsBaseUrl}/collections`,
       },
+      {
+        label: t('Pancake Squad'),
+        href: '/pancake-squad',
+      },
     ],
   },
   {
